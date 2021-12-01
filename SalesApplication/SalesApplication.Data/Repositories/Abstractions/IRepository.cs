88eb73cd2847d6764a47b5a0f359dab53407ca7f
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-12-01 11:49:09 -0300

    TestRepository

---
+++ SalesApplication/SalesApplication.Data/Repositories/Abstractions/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using SalesApplication.Domain.Responses;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System;
+
+namespace SalesApplication.Data.Repositories
+{
+    public interface IRepository<T>
+    {
+        public Task<bool> Save();
+        public Task<IEnumerable<T>> Search();
+        public Task<IEnumerable<T>> Search(Expression<Func<T, bool>> @where);
+        public Task<ActionResponse> Add(T entity);
+    }
+}
---
+++ SalesApplication/SalesApplication.Data/Repositories/TestRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using SalesApplication.Domain;
+using SalesApplication.Domain.Responses;
+
+namespace SalesApplication.Data.Repositories
+{
+    class TestRepository<T> : IRepository<T>
+    {
+        private readonly List<T> _content = new();
+        public async Task<bool> Save()
+        {
+            return await Task.Run(() => true);
+        }
+        public async Task<IEnumerable<T>> Search() 
+        {
+            return await Task.Run(() => _content);
+        }
+        public async Task<IEnumerable<T>> Search(Expression<Func<T, bool>> @where)
+        {
+            return await Task.Run(() => _content.Where(@where.Compile()).ToList());
+        }
+        public async Task<ActionResponse> Add(T entity)
+        {
+            ActionResponse result = new();
+            return await Task.Run(() => {
+                _content.Add(entity);
+                result.Success = true;
+                return result;
+            });
+        }
+    }
+}
