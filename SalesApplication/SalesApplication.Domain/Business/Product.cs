f215c8e9be9eb4db71497660af0250ced2c884df
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-12-01 10:31:32 -0300

    Atualização - Itens de domínio

---
+++ SalesApplication/SalesApplication.Domain/Business/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApplication.Domain.Business
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public Product(
+            string description,
+            double price,
+            int initialStock
+        )
+        {
+            this.Description = description;
+            this.Price = price;
+            this.Stock = initialStock;
+        }
+        public async Task<Customer> Persist()
+        {
+            //TODO: Implementar funcionalidade de persistência no banco de dados
+            return null;
+        }
+    }
+}
---
+++ SalesApplication/SalesApplication.Domain/Business/Sale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApplication.Domain.Business
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+        public List<SoldProduct> Products { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Customer CustomerEntity { get; set; }
+        public int CustomerId { get; set; }
+        public double TotalPrice { get; set; }
+        public Sale(
+            int customerId,
+            double totalPrice,
+            List<SoldProduct> products,
+            DateTime createdAt
+        )
+        {
+            this.CustomerId = customerId;
+            this.TotalPrice = totalPrice;
+            this.Products = products;
+            this.CreatedAt = createdAt;
+        }
+        public async Task<Customer> Persist()
+        {
+            //TODO: Implementar funcionalidade de persistência no banco de dados
+            return null;
+        }
+    }
+}
