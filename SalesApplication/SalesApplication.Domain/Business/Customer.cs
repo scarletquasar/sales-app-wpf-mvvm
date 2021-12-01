166552c4392c01c098c7557fe40d44a363324693
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-12-01 09:52:28 -0300

    Initial commit

---
+++ SalesApplication/SalesApplication.Domain/Business/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SalesApplication.Domain.Business
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Customer(string name)
+        {
+            this.Name = name;
+        }
+        public async Task<Customer> Persist()
+        {
+            //TODO: Implementar funcionalidade de persistência no banco de dados
+            return null;
+        }
+    }
+}
