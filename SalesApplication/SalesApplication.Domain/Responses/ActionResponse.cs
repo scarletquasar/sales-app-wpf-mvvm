b731043f37e465c2f6616dc0d13c54adbe3d9344
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-12-01 11:07:14 -0300

    ActionResponse

---
+++ SalesApplication/SalesApplication.Domain/Responses/ActionResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesApplication.Domain.Responses
+{
+    public class ActionResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
