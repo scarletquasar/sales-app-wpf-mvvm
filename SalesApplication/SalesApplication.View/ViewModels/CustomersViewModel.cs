0999c09adbdb9bd7c43c28314d358a69060a1e05
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-12-06 17:57:17 -0300

    implementation: CustomersViewModel

--- SalesApplication/SalesApplication.View/ViewModels/CustomersViewModel.cs
+++ SalesApplication/SalesApplication.View/ViewModels/CustomersViewModel.cs
@@ -1,13 +1,58 @@
-﻿using System;
+﻿using SalesApplication.Abstractions;
+using SalesApplication.Domain.Business;
+using SalesApplication.Domain.Visualization;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SalesApplication.View.ViewModels
 {
-    public class CustomersViewModel
+    public class CustomersViewModel : INotifyPropertyChanged
     {
-        
+        private readonly IRepository<Customer> _customerRepository;
+        private ObservableCollection<ObservableCustomer> customers;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<ObservableCustomer> Customers
+        {
+            get => customers;
+            set
+            {
+                customers = value;
+                OnPropertyChanged();
+            }
+        }
+        public async Task GetCustomers(string search)
+        {
+            List<Customer> rawCustomers;
+            ObservableCollection<ObservableCustomer> _obsCustomers = new();
+
+            if (uint.TryParse(search, out uint id))
+            {
+                rawCustomers = (await _customerRepository.Search(x => x.Id == id)).ToList();
+            }
+            else
+            {
+                rawCustomers = (await _customerRepository.Search(x => x.Name.Contains(search))).ToList();
+            }
+
+            foreach (Customer item in rawCustomers)
+            {
+                ObservableCustomer result = new();
+                await result.Populate(item.Id, _customerRepository);
+                _obsCustomers.Add(result);
+            }
+
+            Customers = _obsCustomers;
+            OnPropertyChanged();
+        }
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
