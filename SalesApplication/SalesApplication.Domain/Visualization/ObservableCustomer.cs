84e957455d630f939ce1fd9f78a2a62de2198c2f
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-12-06 12:20:21 -0300

    implementation: Customers WIP

---
+++ SalesApplication/SalesApplication.Domain/Visualization/ObservableCustomer.cs
@@ -0,0 +1,31 @@
+﻿using SalesApplication.Abstractions;
+using SalesApplication.Domain.Business;
+using SalesApplication.Domain.Exceptions;
+using SalesApplication.Domain.Hardcodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApplication.Domain.Visualization
+{
+    public class ObservableCustomer
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public async Task Populate(int id, IRepository<Customer> customerRepository)
+        {
+            Customer customer = (await customerRepository.Search(x => x.Id == id)).FirstOrDefault();
+            if (customer != null)
+            {
+                Id = customer.Id;
+                Nome = customer.Name;
+            }
+            else
+            {
+                throw new EntityNotFoundException(ExceptionTexts.EntityNotFound(id.ToString()));
+            }
+        }
+    }
+}
